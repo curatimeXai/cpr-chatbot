525726b1d2c2f452232d8630cdc56f501df4950e
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-09-04 17:23:14 +0200

    Debug

--- analytics/src/index.js
+++ analytics/src/index.js
@@ -4,37 +4,41 @@
  * to display analytics about the bot's use.
  */
 
-import axios from 'axios';
-import cors from "cors";
-import express from 'express';
-import { addUserQuestion, updateBotAnswerCount } from './database.js';
+setInterval(() => {
+	console.log("Je tourne")
+}, 1000);
+
+// import axios from 'axios';
+// import cors from "cors";
+// import express from 'express';
+// import { addUserQuestion, updateBotAnswerCount } from './database.js';
 
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-app.get("/", (req, res) => {
-	res.send("Chatbot API");
-})
+// app.get("/", (req, res) => {
+// 	res.send("Chatbot API");
+// })
 
-app.post('/ask_chatbot', async (req, res) => {
-	try {
-		const chatbotResponse = await axios.post("http://rasa:5005/webhooks/rest/webhook", req.body);
+// app.post('/ask_chatbot', async (req, res) => {
+// 	try {
+// 		const chatbotResponse = await axios.post("http://rasa:5005/webhooks/rest/webhook", req.body);
 		
-		let answer_id = await updateBotAnswerCount(chatbotResponse.data[0].text);
-		await addUserQuestion(req.body.message, answer_id);
+// 		let answer_id = await updateBotAnswerCount(chatbotResponse.data[0].text);
+// 		await addUserQuestion(req.body.message, answer_id);
 
-		return res.json(chatbotResponse.data);
-	}
-	catch (err) {
-		res.status(500).json({
-			message: "Error while sending request to chatbot.",
-			error: err.message,
-		})
-	}
-});
+// 		return res.json(chatbotResponse.data);
+// 	}
+// 	catch (err) {
+// 		res.status(500).json({
+// 			message: "Error while sending request to chatbot.",
+// 			error: err.message,
+// 		})
+// 	}
+// });
 
-app.listen(process.env.API_PORT, () => {
-	console.log("API started!");
-});
+// app.listen(process.env.API_PORT, () => {
+// 	console.log("API started!");
+// });
