cbc70420951ea0e4dba6907827ff5f62401ca646
AtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-09-20 17:21:48 +0200

    Update cors policy.

--- analytics/server/middleware/cors.ts
+++ analytics/server/middleware/cors.ts
@@ -1,14 +1,9 @@
 export default defineEventHandler((event) => {
-	setResponseHeaders(event, {
+	setHeaders(event, {
 		'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		'Access-Control-Allow-Origin': '*',
 		'Access-Control-Allow-Credentials': 'true',
 		'Access-Control-Allow-Headers': '*',
 		'Access-Control-Expose-Headers': '*',
 	});
-	if (event.method === 'OPTIONS') {
-		event.res.statusCode = 204;
-		event.res.statusMessage = 'No Content.';
-		return 'OK';
-	}
 });
